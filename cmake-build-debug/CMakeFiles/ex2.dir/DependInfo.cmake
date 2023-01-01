77e02a408e38211c318f74eb7bf5e5bcdecd1609
liran.redl <liran.redl66666@gmail.com>
2023-01-01 15:45:37 +0200

    FUCNS DONEEEE

---
+++ cmake-build-debug/CMakeFiles/ex2.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/user/CLionProjects/RatuvMivne2/ex2/Player.cpp" "CMakeFiles/ex2.dir/Player.cpp.obj" "gcc" "CMakeFiles/ex2.dir/Player.cpp.obj.d"
+  "C:/Users/user/CLionProjects/RatuvMivne2/ex2/Team.cpp" "CMakeFiles/ex2.dir/Team.cpp.obj" "gcc" "CMakeFiles/ex2.dir/Team.cpp.obj.d"
+  "C:/Users/user/CLionProjects/RatuvMivne2/ex2/UnionFind.cpp" "CMakeFiles/ex2.dir/UnionFind.cpp.obj" "gcc" "CMakeFiles/ex2.dir/UnionFind.cpp.obj.d"
+  "C:/Users/user/CLionProjects/RatuvMivne2/ex2/main23a2.cpp" "CMakeFiles/ex2.dir/main23a2.cpp.obj" "gcc" "CMakeFiles/ex2.dir/main23a2.cpp.obj.d"
+  "C:/Users/user/CLionProjects/RatuvMivne2/ex2/worldcup23a2.cpp" "CMakeFiles/ex2.dir/worldcup23a2.cpp.obj" "gcc" "CMakeFiles/ex2.dir/worldcup23a2.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ex2.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex2.dir/Player.cpp.obj"
+  "CMakeFiles/ex2.dir/Player.cpp.obj.d"
+  "CMakeFiles/ex2.dir/Team.cpp.obj"
+  "CMakeFiles/ex2.dir/Team.cpp.obj.d"
+  "CMakeFiles/ex2.dir/UnionFind.cpp.obj"
+  "CMakeFiles/ex2.dir/UnionFind.cpp.obj.d"
+  "CMakeFiles/ex2.dir/main23a2.cpp.obj"
+  "CMakeFiles/ex2.dir/main23a2.cpp.obj.d"
+  "CMakeFiles/ex2.dir/worldcup23a2.cpp.obj"
+  "CMakeFiles/ex2.dir/worldcup23a2.cpp.obj.d"
+  "ex2.exe"
+  "ex2.exe.manifest"
+  "ex2.pdb"
+  "libex2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
